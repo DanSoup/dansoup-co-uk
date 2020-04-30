31046322051eab9f08b26c2628c52991612c9e0d
DanSoup <dansoup@hotmail.co.uk>
2020-04-30 02:47:20 +0100

    added public folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin =  require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -21,6 +22,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin ({
       template: 'src/index.html'
-    })
+    }),
+    new CopyPlugin([
+      { from: 'src/public', to: 'public' }
+    ])
   ]
 }
