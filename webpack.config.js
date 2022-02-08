35dea2bf734a58330f838ce22ee697da64932d1c
Dan Soup <dansoup@hotmail.co.uk>
2022-02-08 17:52:58 +0000

    fonts are now loaded properly

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,9 @@ module.exports = {
   },
   module: {
     rules: [
-      {test: /\.(js)$/, use:'babel-loader'},
-      {test: /\.s[ac]ss$/i, use:['style-loader', 'css-loader', 'sass-loader']},
+      {test: /\.js$/, use:'babel-loader', exclude: /node_modules/},
+      {test: /\.css$/, use:['style-loader', 'css-loader']},
+      {test: /\.s[ac]ss$/, use:['style-loader', 'css-loader', 'sass-loader']},
       {test: /\.(png|jpe?g|gif)$/i, use:['file-loader']},
       {test: /\.(woff|woff2|eot|ttf|otf)$/i, use:['file-loader']}
     ]
