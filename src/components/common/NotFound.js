5dad6b12c2a68d137fef8aa1fcc23b8f4800eafa
Dan Soup <dansoup@hotmail.co.uk>
2022-07-14 19:39:46 +0100

    update 404 page

--- src/components/common/NotFound.js
+++ src/components/common/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NotFound() {
   return <div id="not-found">
     <h1>404</h1>
-    <p>There is nothing here for you</p>
+    <p>There is nothing here for you yet</p>
   </div>;
 };
 
